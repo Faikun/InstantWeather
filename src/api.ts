006e7cf741e5eb34f6f060aa483fae6164624455
Davron <antius.isakov22@gmail.com>
2023-08-18 15:28:05 +0600

    v1.1

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,5 @@
-const API_KEY = 'bd617664eab24edebcc182814231508'
-const BASE_URL = 'http://api.weatherapi.com/v1'
+const API_KEY = 'PUT YOUR API HERE'
+const BASE_URL = 'https://api.weatherapi.com/v1'
 
 export const getCurrentWeather = async function (city: string) {
   const data = await fetch(`${BASE_URL}/current.json?key=${API_KEY}&q=${city}&aqi=no`)
